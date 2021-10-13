f788e3252a3c4f46f60867aa4ce8c5c7aea01ccb
Илья <46827231+ILYA-DUBINA@users.noreply.github.com>
2021-10-13 09:21:24 +0300

    Add files via upload

---
+++ My_worksAll.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">  
+   <meta name="title" property="og:title" content="MySite">   
+   <meta name="description" property="og:description" content="about me">
+   <meta name="image" property="og:image" content="http://dubina-ilya.site/images/home/I2.png">   
+   <meta property="og:type" content="website"/>   
+   <meta property="og:url" content= "http://dubina-ilya.site/">    
+   
+   <link rel="stylesheet" href="css/style.min.css">
+   <link rel="shortcut icon" href="images/home/знак.jpg">
+   <title>My_site</title>
+</head>   
+<body>  
+   <div class="cocoon">
+      <div class="container">    
+
+         <header class="header">
+            <div class="header__content header__content-myworks">
+               <div class="header__content-image header__content-image-myworks">
+                  <a href="http://dubina-ilya.site/php/admin/entrance_admin.php"><picture><source srcset="images/home/знак.webp" type="image/webp"><img class="header__image-img" src="images/home/знак.jpg" alt="иконка моего сайта"></picture></a>
+               </div>
+               <div class="header__burger">
+                  <span></span>
+               </div>
+               <nav class="header__content-nav header__content-nav-myworks">
+                  <ul class="header__nav-ul">
+                     <li class="header__nav-li">
+                        <a href="http://dubina-ilya.site/index.php" style="background: url('images/home/Shape12.png') 0 0/100% 100% no-repeat;">Главная</a>
+                     </li>
+                     <li class="header__nav-li">
+                        <a href="http://dubina-ilya.site/About_me.php" style="background: url('images/home/Shape12.png') 0 0/100% 100% no-repeat;">Обо мне</a>
+                     </li>
+                     <li class="header__nav-li">
+                        <a href="http://dubina-ilya.site/Contacts.php" style="background: url('images/home/Shape12.png') 0 0/100% 100% no-repeat;">Контактная информация</a>
+                     </li>
+                  </ul>
+               </nav>
+            </div>
+         </header>
+
+         <section class="page myworks">    
+            <div class="myworks__content">
+               <div class="myworks__content-sits all">
+                  <div class="sits__image border-right_down img__free">
+                     <a href="#"><picture><source srcset="images/my_works/00.webp" type="image/webp"><img class="sits__image-img" src="images/my_works/00.png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>
+                  <div class="sits__image border-left_down">
+                     <a href="https://ilya-dubina.github.io/Home/"><picture><source srcset="images/my_works/8-1).webp" type="image/webp"><img class="sits__image-img" src="images/my_works/8-1).png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>
+                  <div class="sits__image border-right_down">
+                     <a href="https://ilya-dubina.github.io/first_template_site/"><picture><source srcset="images/my_works/5-1).webp" type="image/webp"><img class="sits__image-img" src="images/my_works/5-1).png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>                  
+                  <div class="sits__image border-left_down">
+                     <a href="https://ilya-dubina.github.io/What-happens-tomorrow/"><picture><source srcset="images/my_works/6-1).webp" type="image/webp"><img class="sits__image-img" src="images/my_works/6-1).png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>
+                  <div class="sits__image border-right_down">
+                     <a href="https://ilya-dubina.github.io/Alexis/"><picture><source srcset="images/my_works/7-1).webp" type="image/webp"><img class="sits__image-img" src="images/my_works/7-1).png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>              
+                  <div class="sits__image border-left_down">
+                     <a href="http://dinlion-project-concept.space/"><picture><source srcset="images/my_works/2-1).webp" type="image/webp"><img class="sits__image-img" src="images/my_works/2-1).png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>  
+                  <div class="sits__image border-right all__orderDown">
+                     <a href="http://first-restaurant.space/#sidebar"><picture><source srcset="images/my_works/1-1).webp" type="image/webp"><img class="sits__image-img" src="images/my_works/1-1).png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>
+                  <div class="sits__image border-left all__orderUp">
+                     <a href="http://shophia.space/"><picture><source srcset="images/my_works/3-1).webp" type="image/webp"><img class="sits__image-img" src="images/my_works/3-1).png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>                 
+                  <div class="sits__image border-bottom ">
+                     <a href="http://dubina-ilya.site/My_works.php"><picture><source srcset="images/my_works/4-3).webp" type="image/webp"><img class="sits__image-img" src="images/my_works/4-3).png" alt="картинка сайта моей верстки"></picture></a>
+                  </div>  
+               </div>           
+            </div>
+         </section><!?--/section_one-->
+
+         <footer class="footer">
+            <div class="footer__social footer-social-about__me">
+               <ul class="footer__social-ul footer-social-ul-about__me">
+                  <li class="footer__social-li">
+                     <a href="https://twitter.com/Dubina_Ilya"><picture><source srcset="images/global/Twitter.webp" type="image/webp"><img class="footer__social-img" src="images/global/Twitter.png" alt="иконка twitter"></picture></a>
+                  </li>
+                  <li class="footer__social-li">
+                     <a href="https://join.skype.com/invite/HGkYuQt7W98T"><picture><source srcset="images/global/skype2.webp" type="image/webp"><img class="footer__social-img" src="images/global/skype2.png" alt="иконка skype"></picture></a>
+                  </li>
+                  <li class="footer__social-li">
+                     <a href="https://vk.com/id220488562"><picture><source srcset="images/global/vk2.webp" type="image/webp"><img class="footer__social-img" src="images/global/vk2.png" alt="иконка vk"></picture></a>
+                  </li>
+                  <li class="footer__social-li">
+                     <a href="https://www.instagram.com/8ilyadubina8/"><picture><source srcset="images/global/instagram.webp" type="image/webp"><img class="footer__social-img" src="images/global/instagram.png" alt="иконка instagram"></picture></a>
+                  </li>
+                  <li class="footer__social-li">
+                     <a href="https://t.me/Dubina_ilya"><picture><source srcset="images/global/telegram2.webp" type="image/webp"><img class="footer__social-img" src="images/global/telegram2.png" alt="иконка telegram"></picture></a>
+                  </li>
+                  <li class="footer__social-li-line footer__social-li-line-myworks"></li>
+               </ul>
+               <div class="footer__social-image footer-social-image-about__me">
+                  <picture><source srcset="images/home/Shape2-1.webp" type="image/webp"><img class="footer__social-img footer-social-img-about__me" src="images/home/Shape2-1.png" alt="картинка фона социальных сетей"></picture>
+                  <picture><source srcset="images/home/Shape2-3.webp" type="image/webp"><img class="footer__social-img-two" src="images/home/Shape2-3.png" alt="картинка белого пятна"></picture>
+               </div>
+            </div>
+         </footer>
+ 
+      </div> 
+   </div>
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="js/script.min.js"></script>   
+</body>
+</html>
